283631715db10110f8771ab1cecdb14dda2cf549
Nipun Taneja <ntaneja@sfsu.edu>
2025-04-13 18:46:46 -0700

    added home.htm, temp data and api route

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from temp_data import GREETINGS, CHAT_HISTORY
+# import jsonify
 
 app = Flask(__name__)
+
+
+
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html',greetings = GREETINGS, chat_history =CHAT_HISTORY)
+
+@app.route("/api/chatbot")
+def sfsu_api():
+    return jsonify(CHAT_HISTORY)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ temp_data.py
@@ -0,0 +1,11 @@
+GREETINGS = [{
+    'time_of_day': "Afternoon",
+    'name': "Nipun"
+}]
+
+CHAT_HISTORY = [{'date_created': '11/02/1993',
+                 'date_last_modified': '11/02/1993',
+                 'history': [{'prompt1':'answer'},{'prompt2': 'prompt2_answer'}],
+                 'lastprompt':'lastprompt'
+                 
+                 }]
